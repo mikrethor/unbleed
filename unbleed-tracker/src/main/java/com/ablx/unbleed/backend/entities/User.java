f02295648a0587f35192441a71e118e5ef0c3ab2
mikrethor <mikrethor@gmail.com>
2018-05-20 22:12:18 -0400

    Working example to parse json to object

---
+++ unbleed-tracker/src/main/java/com/ablx/unbleed/backend/entities/User.java
@@ -0,0 +1,25 @@
+package com.ablx.unbleed.backend.entities;
+
+public class User {
+	
+	private String name;
+
+	public User() {
+		super();
+	}
+	
+	public User(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
--- unbleed-tracker/src/main/java/com/ablx/unbleed/tracker/handlers/OpenHandler.java
+++ unbleed-tracker/src/main/java/com/ablx/unbleed/tracker/handlers/OpenHandler.java
@@ -4,36 +4,51 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
+import com.ablx.unbleed.backend.entities.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.api.json.JSONConfiguration;
+
 public class OpenHandler {
 
 	@Execute
-	public void execute(Shell shell){
+	public void execute(Shell shell) {
 		try {
-            ClientConfig clientConfig = new DefaultClientConfig();
-           clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-            Client client = Client.create(clientConfig);
+			ClientConfig clientConfig = new DefaultClientConfig();
+			clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+			Client client = Client.create(clientConfig);
 
-            WebResource webResource = client.resource("http://localhost:8080/hello/"+"Xavier");
-            ClientResponse response = webResource.get(ClientResponse.class);
+			WebResource webResource = client.resource("http://localhost:8080/hello/" + "Xavier");
 
-            if (response.getStatus() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
-            }
+			ClientResponse response = webResource.get(ClientResponse.class);
 
-            String output = response.getEntity(String.class);
-System.out.println(output);
+		
+			
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+			}
 
-        } catch (Exception e) {
+			String output = response.getEntity(String.class);
 
-            //Handling errors
+			
+			ObjectMapper mapper = new ObjectMapper();
 
-        }
+			
+
+			// Convert JSON string from file to Object
+			User staff = mapper.readValue(output, User.class);
+			
+System.out.println(staff.getName());
+
+		} catch (Exception e) {
+
+			// Handling errors
+			System.err.println(e);
+		}
 		FileDialog dialog = new FileDialog(shell);
 		dialog.open();
 	}
